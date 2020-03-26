3aa23cc26783770c8c25ed7ddf68f2abf6d364ef
“dziekan30” <dziekan30@gmail.com>
2020-03-26 14:15:12 -0500

    app 100% working

--- src/views/RequestsIndex.vue
+++ src/views/RequestsIndex.vue
@@ -4,61 +4,66 @@
     <!-- Why Choose Us Start -->
     <section class="section_all bg-light">
         <div class="container">
-            <div class="row vertical_content_manage"  v-if="$parent.userId == request.user_id" v-for="request in requests">
-                <div class="col-lg-6">
-                    <div class="description_header_main mt-3" >
-                        <h4 class="text-capitalize font-weight-normal" >Request:</h4>
-                        
-                        <div class="business_choose_box p-3 mt-3">
-                            <div class="business_choose_icon">
-                                <span class=""></span>
-                            </div>
-                            <div class="business_choose_details mt-2">
-                                <h6 class="text-capitalize mb-0">Id: {{request.id}}</h6>
-                            </div>
-                        </div>
+          <div class="row vertical_content_manage section-spacer"  v-if="!requests[0]">
+            <h1 class="align-middle">No Requests</h1>
+          </div>
 
-                        <div class="business_choose_box p-3 mt-3">
-                            <div class="business_choose_icon">
-                                <span class=""></span>
-                            </div>
-                            <div class="business_choose_details mt-2">
-                                <h6 class="text-capitalize mb-0">User Name: {{request.user.name}}</h6>
-                            </div>
-                        </div>
+          <div class="row vertical_content_manage"  v-for="request in requests">
+              <div class="col-lg-6">
+                  <div class="description_header_main mt-3" >
+                      <h4 class="text-capitalize font-weight-normal" >Request:</h4>
+                      
+                      <div class="business_choose_box p-3 mt-3">
+                          <div class="business_choose_icon">
+                              <span class=""></span>
+                          </div>
+                          <div class="business_choose_details mt-2">
+                              <h6 class="text-capitalize mb-0">Id: {{request.id}}</h6>
+                          </div>
+                      </div>
 
-                        <div class="business_choose_box p-3 mt-3">
-                            <div class="business_choose_icon">
-                                <span class=""></span>
-                            </div>
-                            <div class="business_choose_details mt-2">
-                                <h6 class="text-capitalize mb-0">Dog Name: {{request.dog.name}}</h6>
-                            </div>
-                        </div>
+                      <div class="business_choose_box p-3 mt-3">
+                          <div class="business_choose_icon">
+                              <span class=""></span>
+                          </div>
+                          <div class="business_choose_details mt-2">
+                              <h6 class="text-capitalize mb-0">User Name: {{request.user.name}}</h6>
+                          </div>
+                      </div>
 
-                        <div class="business_choose_box p-3 mt-3">
-                            <div class="business_choose_icon">
-                                <span class=""></span>
-                            </div>
-                            <div class="business_choose_details mt-2">
-                                <h6 class="text-capitalize mb-0">Approved: {{ request.approved ? "approved" : "not approved" }}</h6>
-                            </div>
-                        </div>
+                      <div class="business_choose_box p-3 mt-3">
+                          <div class="business_choose_icon">
+                              <span class=""></span>
+                          </div>
+                          <div class="business_choose_details mt-2">
+                              <h6 class="text-capitalize mb-0">Dog Name: {{request.dog.name}}</h6>
+                          </div>
+                      </div>
 
-                        <div class="mt-5">
-                          <router-link v-bind:to="'/requests/' + request.id">
-                            <a href="/#" class="btn btn_custom btn_rounded mr-3">Info</a>
-                          </router-link>
-                        </div>
+                      <div class="business_choose_box p-3 mt-3">
+                          <div class="business_choose_icon">
+                              <span class=""></span>
+                          </div>
+                          <div class="business_choose_details mt-2">
+                              <h6 class="text-capitalize mb-0">Approved: {{ request.approved ? "approved" : "not approved" }}</h6>
+                          </div>
+                      </div>
 
-                    </div>
-                </div>
+                      <div class="mt-5">
+                        <router-link v-bind:to="'/requests/' + request.id">
+                          <a href="/#" class="btn btn_custom btn_rounded mr-3">Info</a>
+                        </router-link>
+                      </div>
+
+                  </div>
+              </div>
               <div class="col-lg-6">
                   <div class="description_img mt-3">
                       <img v-bind:src="request.dog.default_image_url" alt="" class="dog_pict img-fluid mx-auto d-block">
                   </div>
               </div>
-            </div>
+          </div>
+
         </div>
     </section>
     <!-- Descrption end -->
@@ -66,7 +71,10 @@
   </div>
 </template>
 
-<style>
+<style scoped>
+.section-spacer {
+  min-height: 560px;
+}
 </style>
 
 <script>
